1bf531d4e12bb81509144d88b68bde6750ed5439
Aria. KH <khoshnoodaria@outlook.com>
2023-12-24 21:48:12 +0330

    feat: app gitgutter and npairs config

---
+++ .config/nvim/after/plugin/gitgutter.lua
@@ -0,0 +1,8 @@
+vim.cmd([[highlight GitGutterAdd guifg=#009900 ctermfg=Green]])
+vim.cmd([[highlight GitGutterChange guifg=#bbbb00 ctermfg=Green]])
+vim.cmd([[highlight GitGutterChange guifg=#ff2222 ctermfg=Green]])
+
+vim.g.gitgutter_enabled = 1
+vim.g.gitgutter_map_keys = 0
+vim.g.gitgutter_highlight_linenrs = 1
+
