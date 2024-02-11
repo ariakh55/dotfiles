c72dd7a1180da448e4ffe2813d1515753c63a191
Aria KH <khoshnoodaria@gmail.com>
2024-02-11 12:36:58 +0330

    feat: add spotify custom module

---
+++ tmux-catppucin-status/spotify.sh
@@ -0,0 +1,14 @@
+# Requires https://github.com/robhurring/tmux-spotify.
+
+show_spotify() {
+  export MUSIC_APP="spotify"
+
+  local index=$1
+  local icon="$(get_tmux_option "@catppuccin_spotify_icon" "")"
+  local color="$(get_tmux_option "@catppuccin_spotify_color" "$thm_green")"
+  local text="$(get_tmux_option "@catppuccin_spotify_text" "#{music_status} #{track}")"
+
+  local module=$( build_status_module "$index" "$icon" "$color" "$text" )
+
+  echo "$module"
+}
