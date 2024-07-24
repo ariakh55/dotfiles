5756615df539501697e7e85c0acff6ad611eca9e
Aria KH <khoshnoodaria@gmail.com>
2024-07-24 13:38:13 +0330

    feat: better harpoon keybinds

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -5,10 +5,10 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function () harpoon:list():add() end)
 vim.keymap.set("n", "<M-a>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>ht", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>hn", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>P", function() harpoon:list():prev() end)
