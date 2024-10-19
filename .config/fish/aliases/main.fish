a2c26a8116a939244c1196cddd8949065263a27a
Aria KH <khoshnoodaria@gmail.com>
2024-10-19 12:41:12 +0330

    feat: update terminal settings

--- .config/fish/aliases/main.fish
+++ .config/fish/aliases/main.fish
@@ -14,10 +14,16 @@ alias bat="batcat"
 ##better ll
 alias la="exa --icons -la --group-directories-first"
 
+##Projects
+alias newproj="init-project"
+
 ##vim config
 set DOT_DIR "$HOME/.dotconfig/"
 set DOT_WORK_TREE "$HOME"
 set CUSTOM_GIT "GIT_DIR=$DOT_DIR GIT_WORK_TREE=$DOT_WORK_TREE"
 
 alias vimconfig="$CUSTOM_GIT vim ~/.config/nvim"
+alias sshconfig="vim $HOME/.ssh/config"
 alias vidots="$CUSTOM_GIT vim $HOME"
+
+alias supabase="npx supabase"
