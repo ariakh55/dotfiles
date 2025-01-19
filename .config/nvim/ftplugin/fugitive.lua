aa69f4fcd794071d1fc674b9fd62b7edb68da4f0
Aria. KH <khoshnoodaria@outlook.com>
2025-01-19 10:04:52 +0330

    feat: update remote config

---
+++ .config/nvim/ftplugin/fugitive.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>gl", ":Git pull<CR>", { noremap = true });
--- .config/nvim/lua/sulfurchan/commands.lua
+++ .config/nvim/lua/sulfurchan/commands.lua
@@ -1,3 +1,19 @@
+local config_dir = "~/.config/nvim/"
+
+function scan_dir(directory)
+  local i, filename_table, popen = 0, {}, io.popen
+  local pfile = popen('find ' .. directory .. ' -type f -name "*.lua"')
+
+  for filename in pfile:lines() do
+    i = i + 1
+    filename_table[i] = filename:sub(#directory + 2)
+  end
+
+  pfile.close()
+
+  return filename_table
+end
+
 vim.api.nvim_create_user_command('Reload', function(tbl)
     local file
     if tbl.args == nil or tbl.args == '' then
@@ -6,10 +22,35 @@ vim.api.nvim_create_user_command('Reload', function(tbl)
         file = tbl.args
     end
 
-    local configDir = "~/.config/nvim/" .. file
+    local config_file = config_dir  .. file
 
-    local vimCmd = 'so ' .. configDir
+    local vim_cmd = 'so ' .. config_file
 
-    vim.cmd(vimCmd)
-end, {nargs = "*"})
+    vim.cmd(vim_cmd)
+    print("Sourced " .. config_file)
+end, {
+  nargs = "*",
+  complete = function (arg_lead)
+    local directory = vim.fn.stdpath("config")
+    local files = scan_dir(directory)
+    local matches = {}
+
+    for _,file in ipairs(files) do
+      if file:match("^" .. vim.fn.escape(arg_lead,"([%]%)%.%*%+%?%^%$")) then
+        table.insert(matches, file)
+      end
+    end
+
+    return matches
+  end
+})
+
+
+vim.api.nvim_create_user_command('Filename', function(_)
+    local filename = vim.fn.expand('%:t')
+    filename = filename:match("(.+)%..+$") or filename
+
+    vim.api.nvim_put({filename}, 'c', true, true)
+end, {})
+
 
